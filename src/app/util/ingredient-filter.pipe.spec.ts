ceb9a59cc6a46a6ed09716c78d503b161cea2667
Sebastian Soto Madrigal <ssotom@ucenfotec.ac.cr>
2020-11-29 10:56:59 -0600

    removed spec.ts files

--- src/app/util/ingredient-filter.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { IngredientFilterPipe } from './ingredient-filter.pipe';
-
-describe('IngredientFilterPipe', () => {
-  it('create an instance', () => {
-    const pipe = new IngredientFilterPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
