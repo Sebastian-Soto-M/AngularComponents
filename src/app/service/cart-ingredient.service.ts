9e9a75c4e2d2c8c9bd968bdbaa02cd6df853b2a2
Sebastian Soto Madrigal <ssotom@ucenfotec.ac.cr>
2020-12-03 22:47:41 -0600

    feat(ci service): basic methods

--- src/app/service/cart-ingredient.service.ts
+++ src/app/service/cart-ingredient.service.ts
@@ -1,9 +1,59 @@
+import { HttpResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ICartHasIngredient } from '../entities/cart-has-ingredient.model';
+import { ICartIngredient } from '../entities/cart-ingredient.model';
+import { IIngredient } from '../entities/ingredient.model';
+import { Status } from '../status.enum';
+import { CartHasIngredientService } from './cart-has-ingredient.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CartIngredientService {
+  constructor(private chiService: CartHasIngredientService) {}
 
-  constructor() { }
+  create(
+    ci: ICartIngredient,
+    cartId: number
+  ): Observable<HttpResponse<ICartHasIngredient>> {
+    return this.chiService.create({
+      amount: ci.amount,
+      status: ci.status,
+      cartId: cartId,
+      ingredientName: ci.name,
+      ingredientId: ci.id,
+    });
+  }
+
+  update(ci: ICartIngredient): Observable<HttpResponse<ICartHasIngredient>> {
+    return this.chiService.update(ci);
+  }
+
+  toggleStatus(
+    ci: ICartIngredient
+  ): [ICartIngredient, Observable<HttpResponse<ICartHasIngredient>>] {
+    ci.status =
+      ci.status !== Status.PENDING.toUpperCase()
+        ? (Status.PENDING.toUpperCase() as Status)
+        : (Status.ACTIVE.toUpperCase() as Status);
+    return [ci, this.chiService.update(ci)];
+  }
+
+  delete(ci: ICartIngredient): Observable<HttpResponse<ICartHasIngredient>> {
+    return this.chiService.delete(ci.id);
+  }
+
+  map(i: IIngredient, chi: ICartHasIngredient): ICartIngredient {
+    return {
+      id: i.id,
+      name: i.name,
+      amount: chi.amount,
+      image: i.image,
+      imageContentType: i.imageContentType,
+      unitAbbrev: i.unitAbbrev,
+      cartId: chi.cartId,
+      status: chi.status,
+    };
+  }
 }
